cd2c39ee06d7ee234811e372b4a0ae647a498631
vansheffigo <vansh.goyal@effigo.tech>
2024-01-21 17:35:22 +0530

    Adding dependencies for StringUtils

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,13 @@
 package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
+	public int countWords(String words)
+	{
+	String [] seperateWords= StringUtils.split(words,' ');
+	return (seperateWords==null)?0:seperateWords.length;
+	}
     public void greet(){
 	List<String> greetings= new ArrayList<>();
 	greetings.add("HELLO");                                                                  
@@ -21,5 +27,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Word Count :" + count); 
     }
 }
